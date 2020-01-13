8a38d4ca832d63c3e465171840bd54bec10002b6
YarnSeemannsgarn <janschlenker1990@gmail.com>
2020-01-13 18:34:55 +0100

    Fix first draft

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
 from SPARQLWrapper import SPARQLWrapper, JSON
+import os
+from  mycroft.util.log import LOG
 
 class BookDialog(MycroftSkill):
     def __init__(self):
@@ -7,12 +9,17 @@ class BookDialog(MycroftSkill):
 
     @intent_file_handler('what.are.knowledge.graphs.intent')
     def handle_dialog_book(self, message):
-        sparql = open("sparql/what_are_knowledge_graphs.rq")
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        sparql_file = open(dir_path + "/sparql/what_are_knowledge_graphs.rq")
+        sparql = sparql_file.read()
         wrapper = SPARQLWrapper("http://graphdb.sti2.at:8080/repositories/OCWS2019")
+
         wrapper.setQuery(sparql)
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
-        self.speak_dialog('answer', data={"answer": results["results"]["bindings"][0]["comment"]["value"]})
+        wrapper.setReturnFormat(JSON)
+        results = wrapper.query().convert()
+        answer = results["results"]["bindings"][0]["comment"]["value"]
+        
+        self.speak_dialog('answer', data={"answer": answer})
 
 def create_skill():
     return BookDialog()
